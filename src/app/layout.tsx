b90a6fa45e296b6f8c79bd001ad2906d70911823
Elena Barbajosa <elenabarbajosa@Elenas-MacBook-Pro.local>
2026-04-29 01:39:35 +0200

    Add PWA manifest and icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,16 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="shortcut icon" href="/icons/favicon.ico" />
+        <link rel="manifest" href="/icons/site.webmanifest" />
+
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </head>
       <body className="min-h-full flex flex-col bg-zinc-50 text-zinc-900">{children}</body>
     </html>
   );
